ddef52676ffd79e82e0a2ccb2d1b36725fe8e955
TasukuFujiwara <tasuku0221@icloud.com>
2023-05-20 14:23:31 +0900

    Initial Commit

---
+++ AtoDoko/AtoDokoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtoDokoApp.swift
+//  AtoDoko
+//
+//  Created by 藤原輔 on 2023/05/20.
+//
+
+import SwiftUI
+
+@main
+struct AtoDokoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
