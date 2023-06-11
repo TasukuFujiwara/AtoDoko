c4a8134a767cab06dbe2550d18a73ba350bfa830
TasukuFujiwara <tasuku0221@icloud.com>
2023-06-11 17:06:38 +0900

    Add MyMapView: customisable map view

---
+++ AtoDoko/Views/MyMapView.swift
@@ -0,0 +1,43 @@
+//
+//  MyMapView.swift
+//  AtoDoko
+//
+//  Created by 藤原輔 on 2023/06/04.
+//
+
+import SwiftUI
+import MapKit
+
+struct MyMapView: UIViewRepresentable {
+    let myMap = MyMap()
+    var routes: [MKRoute] = []
+
+    init(routes: [MKRoute]) {
+        self.routes = routes.sorted(by: {$0.distance < $1.distance})
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        return myMap.map
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        uiView.removeOverlays(uiView.overlays)
+        uiView.delegate = myMap
+
+        let multiPolyline = MKMultiPolyline(routes.map({$0.polyline}))
+        uiView.addOverlay(multiPolyline, level: .aboveRoads)
+        
+        if let rect = routes.last?.polyline.boundingMapRect {
+            var rectRegion = MKCoordinateRegion(rect)
+            rectRegion.span.latitudeDelta *= 1.5
+            rectRegion.span.longitudeDelta *= 1.5
+            uiView.setRegion(rectRegion, animated: true)
+        }
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyMapView(routes: [])
+    }
+}
